e754d09690154c6db95acb11fe7dd8a504e74311
Tsungyu Yu <ytyubox@gmail.com>
2024-09-30 18:26:05 +0800

    --- Auto Git Commit ---

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -240,8 +240,8 @@ export default class CanvasMindMap extends Plugin {
 						const nodeGroupHeight = (currentSelectionItem.height * 0.6 + 20) * h1list.length;
 						let direction = -1;
 						const nodeGroupY = currentSelectionItem.y + currentSelectionItem.height / 2 + (nodeGroupHeight / 2) * direction;
-						const addCard(node: TreeNode) {
-							createChildCardNode(canvas, currentSelectionItem, item.text, "#" + item, nodeGroupY - direction * (currentSelectionItem.height * 0.6 + 20) * index);
+						const addCard = (node: TreeNode) => {
+							createChildCardNode(canvas, currentSelectionItem, n.text, "#" + item, nodeGroupY - direction * (currentSelectionItem.height * 0.6 + 20) * index);
 						}
 
 						h1list.forEach((item, index) => {
