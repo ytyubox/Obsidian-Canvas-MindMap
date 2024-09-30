d3814f3b2eb354f1ad397b5d6e8c3c13d9b6a908
Tsungyu Yu <ytyubox@gmail.com>
2024-09-30 19:06:27 +0800

    --- Auto Git Commit ---

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -249,7 +249,8 @@ export default class CanvasMindMap extends Plugin {
 						};
 
 						h1list.forEach((item, index) => {
-							const content = item.text + item.content
+							const content = item.text 
+							if 
 							const newCard = createChildCardNode(canvas, currentSelectionItem, item.text, "#" + item, nodeGroupY - direction * (currentSelectionItem.height * 0.6 + 20) * index);
 							addCard(newCard, item)
 						});
