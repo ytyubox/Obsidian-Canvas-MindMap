6b3dd948e71fe8a6284b54b0281e53daea77588c
Tsungyu Yu <ytyubox@gmail.com>
2024-09-30 18:24:35 +0800

    --- Auto Git Commit ---

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -240,7 +240,7 @@ export default class CanvasMindMap extends Plugin {
 						let direction = -1;
 						const nodeGroupY = currentSelectionItem.y + currentSelectionItem.height / 2 + (nodeGroupHeight / 2) * direction;
 
-						addCard() {
+						addCard(node: TreeNode) {
 
 						}
 						h1list.forEach((item, index) => {
