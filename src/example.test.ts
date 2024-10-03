bc9c9e996278bb3b54e05ff41b1d8bf94baeb429
Tsungyu Yu <ytyubox@gmail.com>
2024-10-03 20:36:07 +0800

    TCR

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,10 +1,9 @@
 // example.test.ts
 
-// add.ts
 export const add = (a: number, b: number): number => a + b;
 
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
-  expect(add(1, 2)).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+	expect(add(1, 2)).toBe(3);
+	expect(add(1, 2)).toBe(3);
+	expect(add(1, 2)).toBe(3);
 });
