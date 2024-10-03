c1b26280457ccd35ed80178698340a5c44c23277
Tsungyu Yu <ytyubox@gmail.com>
2024-10-03 20:11:48 +0800

    --- Auto Git Commit ---

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,6 +1,8 @@
 // example.test.ts
 
-import { add } from '../src/add';
+// add.ts
+export const add = (a: number, b: number): number => a + b;
+
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
