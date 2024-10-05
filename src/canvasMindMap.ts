cac27b88f800854ce72eae9d65149b29c32a33a8
Tsungyu Yu <ytyubox@gmail.com>
2024-10-05 13:02:25 +0800

    TCR

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -288,7 +288,7 @@ export default class CanvasMindMap extends Plugin {
 				}
 				// If checking is true, we're simply "checking" if the command can be run.
 				// If checking is false, then we want to actually perform the operation.
-				
+				if 
 				if (!checking) {
 					// @ts-ignore
 					const canvas = canvasView?.canvas;
