f126dc3db604d92d0d4a8786ba0529b4bcab8776
Tsungyu Yu <ytyubox@gmail.com>
2024-10-05 13:09:12 +0800

    TCR 20241005

--- src/Tree.test.ts
+++ src/Tree.test.ts
@@ -25,4 +25,41 @@ test("2 level list from tree", () => {
 	]);
 });
 
-test("complex  tree", () => {})
+test("complex  tree", () => {
+	expect(
+		parseMarkdownToTree(`
+			Direct solution
+Matrix solution
+Particular solution + nullspace solution, x = y + z
+
+[[Vector]]
+[[Linear combination]]
+space
+[[20241002194408|inner product]]
+vector Length
+[[unit vector]]
+Angle between two Vectors
+perpendicular
+Schwarz inequality
+Triangle inequality
+Matrices
+Matrix times vector
+combination of the columns
+Multiplication a row at a time
+Multiplication is also dot product with rows
+
+> Old question: Compute the linear combination $x_1u + x_{2}v + x_{3}w$ to find b.
+> New question: Which combination of $u, v, w$ produces a particular vector b？
+
+[[inverse matrix]]
+integration is the inverse of differentiation.
+[[202409282040|cyclic]]
+All [[Linear combination]] of [[202409282040|cyclic]] vector lie on the plane
+independent columns
+invertible matrix
+dependent column
+singular matrix
+
+			`
+	)).toStrictEqual
+})
