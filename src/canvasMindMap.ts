88a1137a714007db22496e2c874a586ae0847d31
Tsungyu Yu <ytyubox@gmail.com>
2025-03-05 23:40:49 +0800

    TCR Wed Mar  5 23:40:49 CST 2025

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -367,7 +367,7 @@ export default class CanvasMindMap extends Plugin {
 				}
 				// @ts-ignore
 				const mv = this.app.workspace.activeLeaf.view as MarkdownView;
-				console.log(mv);
+
 				if (checking) return true;
 				// console.log("loaded");
 				// @ts-ignore
