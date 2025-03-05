3b9856b03fbcefbc7f3a04c1dd9823a9de54c117
Tsungyu Yu <ytyubox@gmail.com>
2025-03-05 23:09:20 +0800

    TCR Wed Mar  5 23:09:20 CST 2025

--- src/canvasMindMap.ts
+++ src/canvasMindMap.ts
@@ -382,7 +382,7 @@ export default class CanvasMindMap extends Plugin {
 				// console.log("selected", selected);
 
 				const selectedCard = selected.values().next().value;
-				// console.log("selectedCard", selectedCard);
+				console.log("selectedCard", selectedCard);
 				console.log(inspect(selectedCard));
 
 				// get type
