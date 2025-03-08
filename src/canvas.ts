54e9677eb471f78927270677c1916f615df6a888
Tsungyu Yu <ytyubox@gmail.com>
2025-03-08 21:02:59 +0800

    TCR Sat Mar  8 21:02:59 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -57,7 +57,7 @@ export default class BetterCanvas extends Plugin {
 			// if (activeFile === null) {
 			// 	return new Notice("no active file");
 			// }
-			console.log("active file", activeFile);
+			// console.log("active file", activeFile);
 		});
 	}
 }
