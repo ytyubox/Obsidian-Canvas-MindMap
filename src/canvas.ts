947df288e89ec18f714319cf62a5ffb8ee833aaa
Tsungyu Yu <ytyubox@gmail.com>
2025-03-08 21:19:32 +0800

    TCR Sat Mar  8 21:19:32 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -57,8 +57,8 @@ export default class BetterCanvas extends Plugin {
 			}
 
 			if (markdown.length === 0)
-				this.app.workspace.splitActiveLeaf("vertical");
-			return;
+				// this.app.workspace.splitActiveLeaf("vertical");
+				return;
 
 			// if (splitViews.length < 2) {
 			// 	return new Notice("less than 2 split views");
