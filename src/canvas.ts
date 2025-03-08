5f13fddd1f9f1abc77ef6c3dd9ccecc0fa8914c1
Tsungyu Yu <ytyubox@gmail.com>
2025-03-08 21:29:36 +0800

    TCR Sat Mar  8 21:29:36 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -31,7 +31,7 @@ export default class BetterCanvas extends Plugin {
 	registerCommands() {
 		this.app.workspace.on("file-open", (file: TFile | null) => {
 			if (!this.isActive) return;
-			console.log("file-open", file?.extension);
+			console.log("file-open", file?.name);
 
 			const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
 			if (canvasView && canvasView.getViewType() !== "canvas") {
