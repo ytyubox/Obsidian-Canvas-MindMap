c59e6a3fca8ba8252bb2988e3d5f777c2fd71d1e
Tsungyu Yu <ytyubox@gmail.com>
2025-03-08 21:35:03 +0800

    TCR Sat Mar  8 21:35:03 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -56,7 +56,7 @@ export default class BetterCanvas extends Plugin {
 			}
 
 			leaf.openFile(file);
-			this.app.workspace.setActiveLeaf(leaf);
+			this.app.workspace.setActiveLeaf(leaf, { focus: true });
 			return;
 
 			// if (splitViews.length < 2) {
