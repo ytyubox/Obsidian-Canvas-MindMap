7c4c0050d013f7ea1d71766d6d898352a957d8be
Tsungyu Yu <ytyubox@gmail.com>
2025-03-08 21:08:17 +0800

    TCR Sat Mar  8 21:08:17 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -45,7 +45,7 @@ export default class BetterCanvas extends Plugin {
 			if (canvasView?.getViewType() !== "canvas") {
 				return new Notice("not a canvas view");
 			}
-			this.app.workspace.ensureSideLeaf("canvas", "left");
+			this.app.workspace.ensureSideLeaf("canvas", "right");
 			return;
 			console.log("canvas view", canvasView);
 
