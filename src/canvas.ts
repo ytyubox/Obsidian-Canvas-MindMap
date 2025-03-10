43f0ad872c9d8af07ed879acd9bc7885b22f5eed
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 07:23:09 +0800

    TCR Tue Mar 11 07:23:09 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -84,33 +84,32 @@ export default class BetterCanvas extends Plugin {
 		this.app.workspace.on("file-open", this.fromCanvasToSplitMarkdown);
 	}
 	private fromCanvasToSplitMarkdown(file: TFile | null) {
-			if (!this.isActive) return;
-			console.log("file-open", file);
+		if (!this.isActive) return;
+		console.log("file-open", file);
 
-			const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
-			if (canvasView && canvasView.getViewType() !== "canvas") {
-				return;
-			}
-			if (file?.extension !== "md") return;
-			const canvas = this.app.workspace.getLeavesOfType("canvas");
-			if (canvas.length !== 1) {
-				return new Notice("more than 1 canvas view");
-			}
-
-			const markdown = this.app.workspace.getLeavesOfType("markdown");
-
-			if (markdown.length > 1) {
-				return new Notice("more than 1 markdown view");
-			}
-			var leaf: WorkspaceLeaf =
-				markdown.length === 0 && file
-					? this.app.workspace.splitActiveLeaf("vertical")
-					: markdown[0];
-
-			leaf.openFile(file);
-			this.app.workspace.setActiveLeaf(leaf, { focus: true });
+		const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
+		if (canvasView && canvasView.getViewType() !== "canvas") {
 			return;
 		}
+		if (file?.extension !== "md") return;
+		const canvas = this.app.workspace.getLeavesOfType("canvas");
+		if (canvas.length !== 1) {
+			return new Notice("more than 1 canvas view");
+		}
+
+		const markdown = this.app.workspace.getLeavesOfType("markdown");
+
+		if (markdown.length > 1) {
+			return new Notice("more than 1 markdown view");
+		}
+		var leaf: WorkspaceLeaf =
+			markdown.length === 0 && file
+				? this.app.workspace.splitActiveLeaf("vertical")
+				: markdown[0];
+
+		leaf.openFile(file);
+		this.app.workspace.setActiveLeaf(leaf, { focus: true });
+		return;
 	}
 
 	private getNodeFromFile(canvas: Canvas, file: TFile): CanvasNode | null {
