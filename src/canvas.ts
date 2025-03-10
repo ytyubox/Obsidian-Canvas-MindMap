ef1b5c670bd7b8c34eda48c33385994e98ad308c
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 07:00:07 +0800

    TCR Tue Mar 11 07:00:07 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -51,7 +51,7 @@ export default class BetterCanvas extends Plugin {
 				//@ts-ignore
 				const canvas: Canvas = canvasLeaf[0].view.canvas;
 				if (!canvas) return;
-				console.log("canvas", canvas.nodes);
+		
 				var node: CanvasNode | null = null;
 				for (const i of canvas.nodes.values()) {
 					const fnode = i as CanvasFileNode;
