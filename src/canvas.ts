d3533a03e45a20bc08aa697c98adf524829f3f9d
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 06:53:25 +0800

    TCR Tue Mar 11 06:53:25 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -53,7 +53,7 @@ export default class BetterCanvas extends Plugin {
 				if (!canvas) return;
 				console.log("canvas", canvas.nodes);
 				for (const node of canvas.nodes) {
-					console.log("node", node.values);
+					console.log("node", node.values()[1]);
 				}
 
 				const selection: Set<CanvasNode> = canvas.selection;
