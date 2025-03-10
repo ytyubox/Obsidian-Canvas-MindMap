2b3ecae733330e8312838e7c87d9dcc3c167d0c2
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 07:10:20 +0800

    TCR Tue Mar 11 07:10:20 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -86,7 +86,7 @@ export default class BetterCanvas extends Plugin {
 
 				menu.addItem((item) => {
 					item.setIcon("open-in-app");
-					item.setTitle("Split to " + canvas.view.file?.path ");
+					item.setTitle("Split to " + canvas.view.file?.path);
 					item.onClick(() => {
 						// create a new markdown file
 						console.log("split to", canvas.view.file?.name);
