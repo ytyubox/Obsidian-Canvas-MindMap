0ad7170ba26349b30a055427b28eefe572d23c20
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 06:59:13 +0800

    TCR Tue Mar 11 06:59:13 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -63,10 +63,10 @@ export default class BetterCanvas extends Plugin {
 				if (!node) {
 					const selection: Set<CanvasNode> = canvas.selection;
 
-					if (selection.size === 0) {
+					if (selection.size !== 0) {
+						// check if the file is the same as the selected file
+						const selected = Array.from(selection)[0];
 					}
-					// check if the file is the same as the selected file
-					const selected = Array.from(selection)[0];
 				}
 
 				const selection: Set<CanvasNode> = canvas.selection;
