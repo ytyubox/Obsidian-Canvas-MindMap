80bfc6efb9c319ac8ea3f4e8619c2b6f35fa4dae
Tsungyu Yu <ytyubox@gmail.com>
2025-03-11 06:15:50 +0800

    TCR Tue Mar 11 06:15:50 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -100,12 +100,4 @@ export default class BetterCanvas extends Plugin {
 			return;
 		});
 	}
-	registerAllEvents() {
-		this.app.workspace.on("quick-preview", (file: TFile, data: string) => {
-			console.log("quick-preview", file, data);
-		});
-		this.app.workspace.on("editor-change", (editor: Editor) => {
-			console.log("editor-change", editor);
-		});
-	}
 }
