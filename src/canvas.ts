f819e0005d2e8a69ea797fc8a64ba8d6827756cc
Tsungyu Yu <ytyubox@gmail.com>
2025-03-13 08:39:48 +0800

    TCR Thu Mar 13 08:39:47 CST 2025

--- src/canvas.ts
+++ src/canvas.ts
@@ -90,7 +90,7 @@ export default class BetterCanvas extends Plugin {
 		// if the node is not an existed node, then create a new node and add to the center of the canvas
 		console.log("node", node);
 		if (node) {
-			const newNode = createChildFileNode(canvas, node, newFile, null, 0);
+			const newNode = createChildFileNode(canvas, node, newFile, "", 0);
 			console.log("node added");
 			return;
 		}
